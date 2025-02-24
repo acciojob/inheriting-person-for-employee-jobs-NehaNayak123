e4276d0e7adf919c34d38f41734534f9bd3e4e2a
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-02-24 23:14:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,6 +20,27 @@
 // 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 // 	}
 // }
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     greet() {
+//         console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+//     }
+// }
+
+// class Employee extends Person {
+//     constructor(name, age, jobTitle) {
+//         super(name, age);
+//         this.jobTitle = jobTitle;
+//     }
+
+//     jobGreet() {
+//         console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+//     }
+// }
 class Person {
     constructor(name, age) {
         this.name = name;
@@ -27,10 +48,11 @@ class Person {
     }
 
     greet() {
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
     }
 }
 
+// Employee class inheriting from Person
 class Employee extends Person {
     constructor(name, age, jobTitle) {
         super(name, age);
@@ -41,7 +63,6 @@ class Employee extends Person {
         console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
     }
 }
-
 
 // Do not change code below this line
 window.Person = Person;
