49095d3030bdb266059af2f78f4b7607bf245223
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-02-24 22:54:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 // //////////////////////////////////////////
 class Person{
 	constructor(name,age){
-		this.name:name,
-		this.age:age
+		this.name=name,
+		this.age=age
 	}
 	greet(){
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
@@ -14,7 +14,7 @@ class Person{
 class Employee{
 	super(name, age)
 	constructor(jobTitle){
-		this.jobTitle:jobTitle
+		this.jobTitle=jobTitle
 	}
 	jobGreet(){
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
